1473d89df697ba8dc10267f80b1ee3746905b4e8
sclsyin <fy123nig@gmail.com>
2022-10-31 16:28:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sudo rm -rf feeds/packages/lang/golang
+sudo svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
