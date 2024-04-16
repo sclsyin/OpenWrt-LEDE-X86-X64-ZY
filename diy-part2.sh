975330388095cca68a62fe39cfe5275e9251117d
Victor <fy123nig@gmail.com>
2024-04-16 15:35:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,9 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-# sed -i 's/Bootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
-# sed -i 's/Bootstrap/argon/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/Bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/Bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
 
 # Modify hostname
