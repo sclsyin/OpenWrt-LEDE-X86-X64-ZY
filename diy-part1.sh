e4302b8b225143dd88297928e192c6ece686143d
Victor <fy123nig@gmail.com>
2024-11-07 09:12:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+# git clone https://github.com/op4packages/pdnsd-alt package/pdnsd-alt
